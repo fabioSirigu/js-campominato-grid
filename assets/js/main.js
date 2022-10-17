12fc72266d4d43d397260ef24b379589bb81f64b
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-17 16:52:27 +0200

    add select value and generate grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,26 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 
+const gridContainer = document.querySelector('.grid');
 const playButton = document.querySelector('.btn');
 /* const level = document.querySelector('.select_level'); */
 
-let numCell = 100; //nella funzione questo valore voglio che sia dinamico
+// let howMany = 100; //nella funzione questo valore voglio che sia dinamico
 
 //creo una funzione per poterla evocare quando premo il bottone quindi GENERARE la griglia e non nasconderla e farla apparire
-  
-      for (let i = 1; i <= numCell; i++) {
-            const numberInsideCell = i;
-            //creo l'elemento e gli assegno la classe
+ 
+playButton.addEventListener('click', function(){
+      gridContainer.innerHTML= '';
+      const cellNumber = document.querySelector('.form-select').value; // il valore del select lo uso per indicare quante celle voglio nella funzione
+      generateGrid(gridContainer, cellNumber);  
+  });
+      
+
+function generateGrid (where, howMany){
+      for (let i = 0; i < howMany; i++) {
             const cellElement = document.createElement('div');
             cellElement.className = 'cell';
-            // inserisco gli elementi creati dentro al container e dentro gli alementi i numeri
-            const gridContainer = document.querySelector('.grid')
-            gridContainer.append(cellElement);
-            cellElement.append(numberInsideCell);
-      } 
-
-
-
-
-
+            cellElement.innerText = i+1;
+            where.append(cellElement);
+}
+}
